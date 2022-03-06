b7227c7460c40e27b2081c9c693a50344d60db28
special-spoon <enweremchinonyejudith2003@gmail.com>
2022-03-06 14:52:37 +0100

    my second project

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+</head>
+    
+<body>
+ <div class="container">
+    <div class="banner">
+   <div class="nav">
+   <b>Chioma Wedds Nedu</b>
+       <div class="menu">
+       <a href="#">Home</a>
+       <a href="#us">Us</a>    
+       <a href="#venue">Venue</a>    
+       <a href="#party">party</a>    
+       </div>
+   </div>
+        <div class="text">
+        <h1>Chioma & Nedu</h1>
+        <span class="date">On 14th July, 2022</span>
+        </div>
+     </div>
+ </div>
+    
+    <section class="grace">
+   <div class="us">
+    <h2><a name="us">Us</a></h2>
+    <div class="us1">
+    <img src="images/couples.jpeg">
+    <span>Nedu is mine!</span>
+    </div>
+      <div class="us1">
+    <img src="images/nonye.jpg">
+    <span>Chioma is mine!</span>
+      </div>
+   </div>
+        
+    <div class="venue">
+    <h2><a name="venue">Venue</a></h2>
+    <div class="us1">
+    <img src="images/trad.jpg">
+        <p>The white wedding will take place at Saint Pauls Anglican church, Osisioma followed by the traditional <br> marriage at Chief Amadi Okafor's compound,Egbelu in Osisioma L.G.A, Aba, Abia State.</p>
+    </div>
+    </div>
+        
+    <div class="party">
+    <h2><a name="party">Party</a></h2>
+    <div class="us1">
+    <img src="images/wedd%20imgbest.jpg">
+        <p>You wouldn't want to miss this party for anything. Join me as we party on Sunday after the thanks giving service.</p>
+        </div>
+        <footer>&copy; Copyright 2022... <br><i> You can contact me on 08107760484.</i></footer>
+    </div>
+    </section>
+</body>
+</html>
